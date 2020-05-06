cc8815aa074921f131b21fabcd1130b2f9849d16
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-06 15:20:22 +0200

    bug resolved for create controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,21 +2,26 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-end
 
-def create
-    @article = article.new(article_params)
-    if article.save
-        flash[:notice] = "Article créé"
-        redirect_to article_path(@article)
-    else
-        render 'new'
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article créé"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
-end
+
+    def show
+        @article = Article.find(params[:id])
+    end
 
 
-private
+    private
 
-def article_params
-    params.require(:article).permit(:title, :description)
-end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+end 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: {minimum: 5, maximum: 20} 
+    validates :title, presence: true, length: {minimum: 10, maximum: 15} 
     
     validates :description,presence: true, length: {minimum: 20, maximum: 200}
 end
