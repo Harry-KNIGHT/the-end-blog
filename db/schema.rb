76c1a5005fcf2e926a8bd16ddfdb4b9c9acf1276
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-08 09:47:29 +0200

    Create users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_220540) do
+ActiveRecord::Schema.define(version: 2020_05_08_073509) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
