8c1e2bcf6af7a97db29a9dc43b9e85dd483b40ba
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-08 21:07:13 +0200

    User id and some validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    
+    before_save { self.email.downcase! }
     validates :username,
-              presence: true, uniqueness: {case_sensitive: false }, 
-              length: {minimum: 3, maximum: 25}
+    presence: true, uniqueness: { case_sensitive: false },
+    length: {minimum: 3, maximum: 25}
 
-    VALID_EMAIL_REGEX = /.+\@.+\..+/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :email, 
-               uniqueness: {case_sensitive: false }, 
+    validates :email, presence: true, 
+               uniqueness: { case_sensitive: false },
                format: { with: VALID_EMAIL_REGEX }
-
 end
