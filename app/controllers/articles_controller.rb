c5c8158eb24300fc24e330f95f28419c6d3b8fc3
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-08 03:38:57 +0200

    Show page style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        flash[:danger] = "Article supprimé"
+        flash[:danger   ] = "Article supprimé"
         redirect_to articles_path
     end
     private
