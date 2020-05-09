9789dfa28854e5c32e14e5f7e577574a982cf0d4
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-09 16:58:40 +0200

    passwords gem added with users password migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_many :articles
-    
+        
     before_save { self.email.downcase! }
     validates :username,
     presence: true, uniqueness: { case_sensitive: false },
@@ -11,4 +11,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, 
                uniqueness: { case_sensitive: false },
                format: { with: VALID_EMAIL_REGEX }
+
+    has_secure_password
+    
 end
