2e6150d9c358de2c4fc095999084b2a10171d312
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-10 22:21:30 +0200

    bug current user en resolution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user]) if session[:user] 
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_user
+        if not logged_in?
+            flash[:danger] = "T'es pas connecté frérot"
+            redirect_to root_path
+        end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "utilisateur #{@user.username} crée !"
+            flash[:success] = "Utilisateur #{@user.username} crée !"
             redirect_to articles_path
         else
             render 'new'
