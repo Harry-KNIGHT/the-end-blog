98d96597d603942cd2c2d8838844629ef2ef9354
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-12 17:14:24 +0200

    Connexion to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,14 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Utilisateur #{@user.username} crée !"
-            redirect_to articles_path
-        else
-            render 'new'
+            if @user.save
+                session[:user_id] = @user.id #connecte l'user à l'inscription et redirect sur son profil (voir articles_controller/ create  )
+                flash[:success] = "Utilisateur #{@user.username} crée !"
+                redirect_to user_path(@user)
+            else
+                render 'new'
+            end
         end
-    end
 
     def edit
     end
