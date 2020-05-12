389be54005d373e79015445039796f8c2c7ad48c
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-12 22:25:47 +0200

    Show bug fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
     end
 
     
-    private
+    private 
 
     def user_params
         params.require(:user).permit(:username,:email, :password)
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true, length: {minimum: 3, maximum: 25} 
     
-    validates :description,presence: true, length: {minimum: 10, maximum: 200}
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
     validates :user_id, presence: true
 end
